3b579e65fa65362fb41dc4430a076995fe31b704
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-01-23 13:34:38 +0500

    Добавляет отрисовку текста на холсте

--- js/stat.js
+++ js/stat.js
@@ -5,9 +5,12 @@ var CLOUD_COORD_X = 100;
 var CLOUD_COORD_Y = 10;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_TEXT = 'Ура, вы победили! \nСписок результатов: ';
 var CLOUD_FONT_FAMILY = 'PT Mono';
 var CLOUD_FONT_SIZE = '16px';
+var TEXT_COLOR = 'rgba(0, 0, 0, 1)';
 var CLOUD_PADDING = 20;
+var CLOUD_TEXT_PADDING = 30;
 var CLOUD_CONVEX_SIZE = 10;
 
 var IS_SHADOW = true;
@@ -32,7 +35,7 @@ var getRandomNumber = function (min, max) {
 // CHART_BAR_COLORS[getRandomNumber(0, CHART_BAR_COLORS.length - 1)];
 
 // Рисует окно для сообщения
-var drawCloud = function (ctx, color, coordX, coordY, width, height, shadow = false) {
+var renderCloud = function (ctx, color, coordX, coordY, width, height, shadow = false) {
   var coordinateX = shadow ? (coordX += SHADOW_OFFSET) : coordX;
   var coordinateY = shadow ? (coordY += SHADOW_OFFSET) : coordY;
 
@@ -40,7 +43,21 @@ var drawCloud = function (ctx, color, coordX, coordY, width, height, shadow = fa
   ctx.fillRect(coordinateX, coordinateY, width, height);
 };
 
+var renderCongratulation = function (ctx, text, fontData, coordinates) {
+  ctx.fillStyle = fontData.color;
+  ctx.font = fontData.size + ' ' + fontData.family;
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(text, coordinates.x, coordinates.y);
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  drawCloud(ctx, SHADOW_COLOR, CLOUD_COORD_X, CLOUD_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, IS_SHADOW);
-  drawCloud(ctx, CLOUD_COLOR, CLOUD_COORD_X, CLOUD_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, SHADOW_COLOR, CLOUD_COORD_X, CLOUD_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, IS_SHADOW);
+  renderCloud(ctx, CLOUD_COLOR, CLOUD_COORD_X, CLOUD_COORD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+  renderCongratulation(
+    ctx,
+    CLOUD_TEXT,
+    {size: CLOUD_FONT_SIZE, family: CLOUD_FONT_FAMILY, color: TEXT_COLOR},
+    {x: CLOUD_COORD_X + CLOUD_TEXT_PADDING, y: CLOUD_COORD_Y + CLOUD_PADDING}
+  );
 };
